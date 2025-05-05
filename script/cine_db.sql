6d520c02131cca98c1ff98d8b816cd70f08e9c01
bryan <Bjcabello55@gmail.com>
2025-05-05 06:35:54 -0500

    error

--- script/cine_db.sql
+++ script/cine_db.sql
@@ -1,8 +1,9 @@
 CREATE TABLE pelicula (
   id_pelicula SERIAL PRIMARY KEY,
   nombre VARCHAR(100) NOT NULL,
-  duracion INT NOT NULL,
-  estado BOOLEAN DEFAULT TRUE
+  descripcion TEXT,
+  duracion INT,
+  fecha_publicacion DATE
 );
 
 CREATE TABLE sala_cine (
@@ -13,8 +14,8 @@ CREATE TABLE sala_cine (
 
 CREATE TABLE pelicula_sala_cine (
   id_pelicula_sala SERIAL PRIMARY KEY,
-  id_pelicula INT REFERENCES pelicula(id_pelicula),
-  id_sala_cine INT REFERENCES sala_cine(id_sala),
+  id_pelicula INT REFERENCES pelicula(id_pelicula) ON DELETE CASCADE,
+  id_sala_cine INT REFERENCES sala_cine(id_sala) ON DELETE CASCADE,
   fecha_publicacion DATE,
   fecha_fin DATE
 );
