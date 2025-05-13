cb93ca0e10d07d047cdfde9d9232ae54de6c0fc8
bryan <Bjcabello55@gmail.com>
2025-05-13 11:12:40 -0500

    agregando front del dasboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
   totalSalas: number = 0;
   salasDisponibles: number = 0;
   totalPeliculas: number = 0;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    // Simulación de datos por ahora
-    this.totalSalas = 10;
-    this.salasDisponibles = 6;
-    this.totalPeliculas = 15;
+    this.obtenerIndicadores();
+  }
+
+  obtenerIndicadores(): void {
+    this.http.get<any>('http://localhost:3000/dashboard/indicadores')
+      .subscribe(data => {
+        this.totalSalas = data.totalSalas;
+        this.salasDisponibles = data.salasDisponibles;
+        this.totalPeliculas = data.totalPeliculas;
+      });
   }
 }
